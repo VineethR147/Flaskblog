409450fe4691090bfc257155641614ce455de556
VineethR147 <vineeth.181ee254@nitk.edu.in>
2019-10-12 18:43:00 +0530

    templates and basic html and css done

---
+++ flaskr/_init_.py
@@ -0,0 +1,30 @@
+from flask import Flask , render_template, url_for 
+app = Flask(__name__)
+
+
+posts = [
+	{
+	'author':'Vineeth',
+	'title':'BLog-Post 1',
+	'content':'First post content',
+	'date_posted':'14-01-2000'	
+	},
+	{
+	'author':'Suhith',
+	'title':'BLog-Post 2',
+	'content':'second post content',
+	'date_posted':'02-09-2000'	
+	}
+]
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html',title='about')
+
+if __name__ == '__main__' :
+    app.run(debug=True)
