037b0bc12a35644bdc6836ee8fe14386155786ac
VineethR147 <vineeth.181ee254@nitk.edu.in>
2019-10-15 04:57:41 +0530

    Forms

--- flaskr/_init_.py
+++ flaskr/_init_.py
@@ -1,21 +1,25 @@
-from flask import Flask , render_template, url_for 
-app = Flask(__name__)
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 posts = [
-	{
-	'author':'Vineeth',
-	'title':'BLog-Post 1',
-	'content':'First post content',
-	'date_posted':'14-01-2000'	
-	},
-	{
-	'author':'Suhith',
-	'title':'BLog-Post 2',
-	'content':'second post content',
-	'date_posted':'02-09-2000'	
-	}
+    {
+        'author': 'VIneeth',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': '69/69/69'
+    },
+    {
+        'author': 'Suhith',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': '420/420/420'
+    }
 ]
+
+
 @app.route("/")
 @app.route("/home")
 def home():
@@ -24,7 +28,29 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template('about.html',title='about')
+    return render_template('about.html', title='About')
 
-if __name__ == '__main__' :
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
     app.run(debug=True)
